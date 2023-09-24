2f389c0db12fcab67e820ab041668263a3faab2a
GC-223 <gc223odin@gmail.com>
2023-09-24 10:57:48 -0700

    Initial Commit

---
+++ Chip_8_Emu_C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chip_8_Emu_C++
+//
+//  Created by Greyson Chavez on 9/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
